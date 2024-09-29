0e12f5c134e9c45e976abc543486401b9f9c1b55
Sohel Rana <mdsrana11@gmail.com>
2024-09-30 00:00:05 +0600

    Learn Middleware(run, custom middleware, middleware chaing, middleware class)

--- MiddlwareExample/MiddlwareExample/Program.cs
+++ MiddlwareExample/MiddlwareExample/Program.cs
@@ -1,12 +1,27 @@
-using System;
+using MiddlwareExample.CustomMiddleware;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddleware>();
 var app = builder.Build();
 
-app.Run(async (HttpContext context) =>
+// Middleware1
+app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Hello MiddleWare");
+
+    await context.Response.WriteAsync("Hello Middleware1 - ");
+    await next(context); // Passes control to the next middleware
 });
 
+// Middleware2
+app.UseMiddleware<MyCustomMiddleware>();
+
+//Now I am using extention method to invoke the custom middleware
+//app.UseMyCustomMiddleware();
+
+// Middleware3 (Terminal middleware)
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync(" Middleware3");
+});
 
 app.Run();
