77cc642679c66aca19362fac262c35c857bb2a5b
Sohel Rana <mdsrana11@gmail.com>
2024-10-05 02:03:39 +0600

    Learn Route constraint (int, double, bool, range, alpha, regex).

---
+++ RoutingExample/RoutingExample/CustomMinLengthRouteConstraint .cs
@@ -0,0 +1,26 @@
+﻿
+namespace RoutingExample
+{
+    public class CustomMinLengthRouteConstraint : IRouteConstraint
+    {
+        private readonly int _minLength;
+
+        public CustomMinLengthRouteConstraint(int minLength)
+        {
+            _minLength = minLength;
+        }
+
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if (values.TryGetValue(routeKey, out var value) && value != null)
+            {
+                var stringValue = Convert.ToString(value);
+                if (!string.IsNullOrEmpty(stringValue) && stringValue.Length >= _minLength)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- RoutingExample/RoutingExample/Program.cs
+++ RoutingExample/RoutingExample/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing.Constraints;
+
 namespace RoutingExample
 {
     public class Program
@@ -61,8 +64,8 @@ namespace RoutingExample
                     await context.Response.WriteAsync($"I am employee - {empId}");
                 });
 
-                // Default parameter
-                endpoints.Map("product/details/{Id?}", async (context) =>
+                // Default parameter and optional parameter! min constraint will accept the matched values
+                endpoints.Map("product/details/{Id:Range(2, 6)}", async (context) =>
                 {
                     if (context.Request.RouteValues.ContainsKey("Id"))
                     {
@@ -71,9 +74,45 @@ namespace RoutingExample
                     }
                     else
                     {
-                        await context.Response.WriteAsync($"Product id is no provided!");
+                        await context.Response.WriteAsync($"Product id is not provided!");
                     }
                 });
+
+                //Ex: the route constraint is datetime
+                endpoints.Map("daily-product-report/{reportDate:datetime}", async (context) =>
+                {
+                    DateTime dateTime = Convert.ToDateTime(context.Request.RouteValues["reportDate"]);
+                    await context.Response.WriteAsync($"Daily Product Report Date: {dateTime}");
+                });
+
+                //Ex: the route constraint is Guid
+                endpoints.Map("cities/{guidId:guid}", async (context) =>
+                {
+                    Guid guid = Guid.Parse(Convert.ToString(context.Request.RouteValues["guidId"]));
+                    await context.Response.WriteAsync($"The city Guid id is: {guid}");
+                });
+
+                // Using the 'minlength' constraint to enforce a minimum length of 3 characters
+                endpoints.Map("employee/profile/{empName:length(3,6)=sohel}", async context =>
+                {
+                    string? name = Convert.ToString(context.Request.RouteValues["empName"]);
+                    await context.Response.WriteAsync($"The employee name is - {name}");
+                });
+
+                // Route that uses the 'alpha' constraint to allow only alphabetic characters for empName
+                endpoints.Map("employee/Alphaname/{empName:alpha}", async context =>
+                {
+                    string? name = Convert.ToString(context.Request.RouteValues["empName"]);
+                    await context.Response.WriteAsync($"The employee name is with Alpha - {name}");
+                });
+
+                // Route that uses the 'regex' constraint to allow alphabetic characters (A-Z, a-z) and spaces
+                endpoints.Map("employee/Regname/{empName:regex(^[a-zA-Z\\s]+$)}", async context =>
+                {
+                    string? name = Convert.ToString(context.Request.RouteValues["empName"]);
+                    await context.Response.WriteAsync($"The employee name is - {name}");
+                });
+
             });
 
             //making default endpoint
