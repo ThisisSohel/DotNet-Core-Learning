e06945e001afd74031fb6e1ac1c14224fd5df14e
Sohel Rana <mdsrana11@gmail.com>
2024-10-10 19:37:37 +0600

    Add some validation rules and custom validations

--- ModelValidationExample/ModelValidationExample/Models/Person.cs
+++ ModelValidationExample/ModelValidationExample/Models/Person.cs
@@ -1,23 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace ModelValidationExample.Models
 {
     public class Person
     {
 
-        [Required (ErrorMessage ="Name can't be empty")]
-        [MaxLength(10, ErrorMessage = "Name length can be more than 10")]
-        [MinLength(3, ErrorMessage = "Name length can be less than 3")]
+        [Required(ErrorMessage = "{0} can't be empty")]
+        [Display(Name = "Person Name")]
+        [RegularExpression("^[A-Za-z .]$", ErrorMessage = "{0} is not in correct format!")]
+        //[MaxLength(10, ErrorMessage = "{0} length can be more than 10")]
+        //[MinLength(3, ErrorMessage = "{0} length can be less than 3")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "{0} can be more than {1} and less than {2}")]
         public string? PersonName { get; set; }
-        [Required]
+
+        [EmailAddress(ErrorMessage = "{0} is not in correct format!")]
         public string? Email { get; set; }
-        [Required]
+
+        [Phone(ErrorMessage = "{0} is not in correct format")]
+        [ValidateNever] //It is mean this field will be validate!
         public string? Phone { get; set; }
-        [Required]
+        [Required(ErrorMessage = "{0} can not be blank!")]
         public string? Password { get; set; }
-        [Required]
+        [Required(ErrorMessage = "{0} can not be blank")]
+        [Compare("Password", ErrorMessage = "{1} and {0} are not matched!")]
         public string? ConfirmPassword { get; set; }
-        [Required]
+
+        [Range(0, 999.9, ErrorMessage ="{0} can not be more than {2} and less than {1}")]
         public double? Price { get; set; }
 
         public override string ToString()
