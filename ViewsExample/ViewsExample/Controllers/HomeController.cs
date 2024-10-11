eba94ae59be8762b3fcb148e92897998c73decfe
Sohel Rana <mdsrana11@gmail.com>
2024-10-12 02:32:06 +0600

    Start to learn View Part. Write some code for view to see how it works. Write some block and in the block used the if else statement and local variables

---
+++ ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            //return Content("Index");
+            //return new ViewResult()
+            //{
+            //    ViewName = "Home",
+            //};
+            return View();
+        }
+    }
+}
---
+++ ViewsExample/ViewsExample/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace ViewsExample.Models
+{
+    public class Person
+    {
+        public string PersonName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
