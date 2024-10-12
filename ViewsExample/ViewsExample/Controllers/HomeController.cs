2a2232f7e6598074fe562f46c2a56a7d98892214
Sohel Rana <mdsrana11@gmail.com>
2024-10-12 23:16:44 +0600

    Learn about viewdata, viewbag, and strongly typed view with code implementation

--- ViewsExample/ViewsExample/Controllers/HomeController.cs
+++ ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewsExample.Models;
 
 namespace ViewsExample.Controllers
 {
@@ -8,12 +9,75 @@ namespace ViewsExample.Controllers
         [Route("/")]
         public IActionResult Index()
         {
-            //return Content("Index");
-            //return new ViewResult()
-            //{
-            //    ViewName = "Home",
-            //};
-            return View();
+            List<Person> personList = new List<Person>
+            {
+                new Person()
+                {
+                    PersonName = "Name1", DateOfBirth = Convert.ToDateTime("2020-02-02"), PersonGender = Gender.Male,
+                },
+                new Person()
+                {
+                    PersonName = "Name2", DateOfBirth = Convert.ToDateTime("2022-02-02"), PersonGender = Gender.Male,
+                },
+                new Person()
+                {
+                    PersonName = "Name3", DateOfBirth = Convert.ToDateTime("2021-02-02"), PersonGender = Gender.Male,
+                },
+                        new Person()
+                {
+                    PersonName = "Name4", DateOfBirth = Convert.ToDateTime("2010-02-02"), PersonGender = Gender.Male,
+                },
+                new Person()
+                {
+                    PersonName = "Name5", DateOfBirth = Convert.ToDateTime("2019-02-02"), PersonGender = Gender.Male,
+                },
+                new Person()
+                {
+                    PersonName = "Name6", DateOfBirth = Convert.ToDateTime("2021-02-02"), PersonGender = Gender.Male,
+                },
+            };
+            ViewData["person"] = personList;
+            ViewData["appTitle"] = "ASP .NET Core App";
+            ViewData["bodyMessage"] = "Welcome to the App!";
+            return View(personList);
+        }
+
+        [Route("personDetails/{name}")]
+        public IActionResult Details(string name)
+        {
+            if (name == null)
+                return Content("Name can not be empty!");
+
+            List<Person> personList = new List<Person>
+            {
+                new Person()
+                {
+                    PersonName = "Name1", DateOfBirth = Convert.ToDateTime("2020-02-02"), PersonGender = Gender.Male,
+                },
+                new Person()
+                {
+                    PersonName = "Name2", DateOfBirth = Convert.ToDateTime("2022-02-02"), PersonGender = Gender.Male,
+                },
+                new Person()
+                {
+                    PersonName = "Name3", DateOfBirth = Convert.ToDateTime("2021-02-02"), PersonGender = Gender.Male,
+                },
+                        new Person()
+                {
+                    PersonName = "Name4", DateOfBirth = Convert.ToDateTime("2010-02-02"), PersonGender = Gender.Male,
+                },
+                new Person()
+                {
+                    PersonName = "Name5", DateOfBirth = Convert.ToDateTime("2019-02-02"), PersonGender = Gender.Male,
+                },
+                new Person()
+                {
+                    PersonName = "Name6", DateOfBirth = Convert.ToDateTime("2021-02-02"), PersonGender = Gender.Male,
+                },
+            };
+
+            Person? personMatch = personList.Where(per => per.PersonName == name).FirstOrDefault();
+            return View(personMatch);
         }
     }
 }
