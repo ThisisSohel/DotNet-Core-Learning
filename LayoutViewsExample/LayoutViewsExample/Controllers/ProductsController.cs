2db60aacf459a3649b6549bba708cf289776886d
Sohel Rana <mdsrana11@gmail.com>
2024-10-16 00:00:09 +0600

    Learn about Layout View

---
+++ LayoutViewsExample/LayoutViewsExample/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewsExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [Route("/products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/search")]
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [Route("/order")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
