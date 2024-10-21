2583ad2b13ee980c094089438ee9193671f4b32e
Sohel Rana <mdsrana11@gmail.com>
2024-10-21 11:11:22 +0600

    Learn PartialViews (Partial view, View with viewbag)

---
+++ PartialViewsExample/PartialViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartialViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["ListTitle"] = "Cities";
+            ViewData["ListItem"] = new List<string>()
+            {
+                "Dhaka",
+                "Rajshahi",
+                "Rajbari",
+                "Sylhet"
+            };
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
